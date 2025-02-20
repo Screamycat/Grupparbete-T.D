c8f8c2ca290eaa45bee1d7169d90f5007a20309c
Tobias <screamys.cat@gmail.com>
2025-02-20 15:30:38 +0100

    Start

---
+++ Projekt-2.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Given data
+E_modul = 210e9  # [Pa]
+sigma_s = 230e6 # [Pa]
+L = 2.5  # [m]
+A_0 = 78.5e-1 # [m^2]
+A_4_6_9 = 2 * A_0
+P = 150e3  # [N]
