a1438aa84011defb50da98c60e4ba36354e40837
Tobias <screamys.cat@gmail.com>
2025-03-26 16:17:13 +0100

    start

--- Håll.F/Uppgift_1.py
+++
@@ -1,9 +0,0 @@
-# -------------------------------------------
-# Authors: Tobias Ericsson, David Neinhardt |
-# Date: 2025-03-27                          |
-# Course Code: MTM026                       |
-# -------------------------------------------
-# Projektuppgift 1 - Kontinuerlig balk
-
-import pylab as pl
-import sumpy as sp
